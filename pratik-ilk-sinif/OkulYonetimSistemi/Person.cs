8bdcccf3f6eebeb99bdd3e11d64d9a30c03cb90f
OrkunDemir5 <orkun.d1999@gmail.com>
2024-10-18 14:34:26 +0300

    pratik-ilk-sinif

---
+++ pratik-ilk-sinif/OkulYonetimSistemi/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OkulYonetimSistemi
+{
+    public class Person
+    {
+        private string name;
+        private string surname;
+        private DateTime birthday;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                surname = value;
+            }
+        }
+
+        public DateTime Birthday
+        {
+            get
+            {
+                return birthday;
+            }
+            set
+            {
+                birthday = value;
+            }
+        }
+        public void EkranaYazdir()
+        {
+            Console.WriteLine($"Ad: {Name}, Soyad: {Surname}, Doğum Tarihi: {Birthday.ToShortDateString()}");
+        }
+    }
+}
---
+++ pratik-ilk-sinif/OkulYonetimSistemi/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace OkulYonetimSistemi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person ogrenci1 = new Person();
+            Person ogrenci2 = new Person();
+
+            Person ogretmen1 = new Person();
+            Person ogretmen2 = new Person();
+
+            ogrenci1.Name = "Orkun";
+            ogrenci1.Surname = "Demir";
+            ogrenci1.Birthday = new DateTime(1999, 8, 9);
+
+            ogrenci2.Name = "Umur";
+            ogrenci2.Surname = "Demir";
+            ogrenci2.Birthday = new DateTime(1999, 9, 22);
+
+            ogretmen1.Name = "İsmail";
+            ogretmen1.Surname = "Sağıroğlu";
+            ogretmen1.Birthday = new DateTime(1974, 5, 14);
+
+            ogretmen2.Name = "Murat";
+            ogretmen2.Surname = "Algül";
+            ogretmen2.Birthday = new DateTime(1973, 3, 30);
+
+            Console.WriteLine("Öğrenci Bilgileri: ");
+            ogrenci1.EkranaYazdir();
+            ogrenci2.EkranaYazdir();
+
+            Console.WriteLine("Öğretmen Bilgileri: ");
+            ogretmen1.EkranaYazdir();
+            ogretmen2.EkranaYazdir();
+
+        }
+    }
+}
