e0f6732b0ddc060a65fd2737c9faf0f5fb8bfa7d
Orkun Demir <91637132+OrkunDemir5@users.noreply.github.com>
2024-10-18 14:38:37 +0300

    Program.cs

--- pratik-ilk-sinif/OkulYonetimSistemi/Program.cs
+++ pratik-ilk-sinif/OkulYonetimSistemi/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace OkulYonetimSistemi
+//Class'ı çağırıyoruz
+namespace OkulYonetimSistemi
 {
     internal class Program
     {
@@ -9,7 +10,7 @@
 
             Person ogretmen1 = new Person();
             Person ogretmen2 = new Person();
-
+            //Öğrenci Bilgileri
             ogrenci1.Name = "Orkun";
             ogrenci1.Surname = "Demir";
             ogrenci1.Birthday = new DateTime(1999, 8, 9);
@@ -17,7 +18,7 @@
             ogrenci2.Name = "Umur";
             ogrenci2.Surname = "Demir";
             ogrenci2.Birthday = new DateTime(1999, 9, 22);
-
+            //Öğretmen Bilgileri
             ogretmen1.Name = "İsmail";
             ogretmen1.Surname = "Sağıroğlu";
             ogretmen1.Birthday = new DateTime(1974, 5, 14);
@@ -26,10 +27,12 @@
             ogretmen2.Surname = "Algül";
             ogretmen2.Birthday = new DateTime(1973, 3, 30);
 
+            //Öğrenci bilgileri ekrana yazdır
             Console.WriteLine("Öğrenci Bilgileri: ");
             ogrenci1.EkranaYazdir();
             ogrenci2.EkranaYazdir();
 
+            // Öğretmen bilgileri ekrana yazdır
             Console.WriteLine("Öğretmen Bilgileri: ");
             ogretmen1.EkranaYazdir();
             ogretmen2.EkranaYazdir();
